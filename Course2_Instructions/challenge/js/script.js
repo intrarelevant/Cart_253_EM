2635986bc77ece9a8eebf48a121e3ef6f66e7926
intrarelevant <erica.t.mercier@gmail.com>
2026-09-16 19:23:59 -0400

    Experimenting with the background color
    Tried and failed to get paletteLerp to work.

--- Course2_Instructions/challenge/js/script.js
+++ Course2_Instructions/challenge/js/script.js
@@ -8,17 +8,24 @@
 "use strict";
 
 /**
- * Setup to create a canvas of (x, y)
+ * Setup to create a canvas normalized to the viewer with a background color that varies between two shades of blue
+ * NEED TO FIX THIS
 */
 function setup() {
- createCanvas(720, 1280);
+ createCanvas(windowWidth, windowHeight);
+let bg1 = color(179,214,198);
+let bg2 = color(168, 204, 201)
+background(paletteLerp([
+    [bg2, 0.05],
+    [bg1, 1],
+  ], millis() / 10000 % 1))
 }
 
-
 /**
- * Beautiful Pink Background and rectangle
+ * The "Sun"
 */
 function draw() {
- background(255, 100, 100);
+ fill(199, 214, 109);
  ellipse(240, 300, 240, 240);
+ noStroke();
 }
