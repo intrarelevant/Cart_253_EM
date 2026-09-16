67fddb774bbe42aa1a8e01e6402c02f9f756069a
intrarelevant <erica.t.mercier@gmail.com>
2026-09-16 19:47:42 -0400

    A landscape is starting to emerge
    :)

--- Course2_Instructions/challenge/js/script.js
+++ Course2_Instructions/challenge/js/script.js
@@ -8,24 +8,28 @@
 "use strict";
 
 /**
- * Setup to create a canvas normalized to the viewer with a background color that varies between two shades of blue
- * NEED TO FIX THIS
+ * Setup, Canvas should be height and width of browser window
 */
 function setup() {
  createCanvas(windowWidth, windowHeight);
+ imageMode(CENTER)
 let bg1 = color(179,214,198);
-let bg2 = color(168, 204, 201)
-background(paletteLerp([
-    [bg2, 0.05],
-    [bg1, 1],
-  ], millis() / 10000 % 1))
+let bg2 = color(168, 204, 201);
+angleMode(DEGREES);
+background(bg2)
 }
 
-/**
- * The "Sun"
-*/
+
 function draw() {
- fill(199, 214, 109);
- ellipse(240, 300, 240, 240);
+    let c1 = color(199, 214, 109); /*darkgreen*/
+    let c2 = color(220, 234, 178); /*teagreen*/
+    
+ /*** draw ellipse as the sun for the landscape*/
+ fill(c1);
  noStroke();
+ ellipse(windowWidth/2, windowHeight/2, 600, 400);
+  /* draw horizon line */
+ fill(c2);
+ noStroke();
+ rect(0, windowHeight*0.7, windowWidth, windowHeight);
 }
