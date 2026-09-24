3f483c9c6c3c603d5f1a380b8ddf394df3088c07
intrarelevant <erica.t.mercier@gmail.com>
2026-09-24 12:56:48 -0400

    Implement scale motion, color, feedback effect
    Mostly complete prototype! I could further implement letter rotation, scale, etc... but I think this suffices for now :)

--- Course3_Variables/assignment/prototype/js/script.js
+++ Course3_Variables/assignment/prototype/js/script.js
@@ -7,9 +7,6 @@
 
 "use strict";
 
-// setting up a variable to keep track of time
-let absTime = 0;
-
 /*** Create the canvas*/
 function setup() {
   createCanvas(800, 800);
@@ -25,11 +22,11 @@ let myColors = {
     bg: { // background color
         r:224, g:216, b:222 },// "Lavender Blush"
 
-    maintext: { //color for the main text
-      r:106, g:98, b:98 }, // "Dim Grey"
+    text1: {
+        r:75, g:59, b:71}, // mauve
 
-    activetext: { //color for the changing letters
-      r:207, g:210, b:178 }, // "Sand Dune" kind of like green
+    text2: {
+      r:166, g:172, b:114} // Sage
 }
 
 // 2: I want the individual letters to wiggle around at an even pace and be able to control the motion in one place
@@ -43,8 +40,8 @@ let myColors = {
 //sin(...) creates smooth, continuous movement (in the function)
 
 let letterWiggle = {
-  y: {amp:20, speed:0.05},
-  s: {amp:5, speed:0.04}
+  y: {amp:40, speed:0.03},
+  s: {amp:4, speed:0.03}
   }
 
 // 3: Rearranging the letters
@@ -53,19 +50,19 @@ let wordLetters = { // I want to act on each letter individually, these are the 
     pos2: {x:300, y:400},
     pos3: {x:500, y:400},
     pos4: {x:600, y:400},
-    abs: { // but I will also need an absolute origin that doesn't change.
+    abs: { // I also need an absolute target position that doesn't change.
           pos1: {x:200, y:400},
           pos2: {x:350, y:400},
-          pos3: {x:450, y:400},
+          pos3: {x:450, y:400}, // pos 2 and 3 are intentioally not the same
           pos4: {x:600, y:400},
-          xtranslate: 1.02 // speed of horizontal motion
+          xtranslate: 1.02, // speed of horizontal motion, will be maintained even
+          textsize: 12, // starting text size
     }
   }
 
 /// Draw starts and runs every frame
 function draw() {
-absTime = frameCount/600 ;// duration - 10 seconds // I want absTime to function as a timer if I need it.
-background(myColors.bg.r, myColors.bg.g, myColors.bg.b); // apply the background color 
+background(myColors.bg.r, myColors.bg.g, myColors.bg.b, 120*random(0.95,1.05)); // apply the background color and but with low opacity to create feedback.
 
 // I want the letters to be constrained to their ending positions in terms of the x axis.
 // W pos 1 -> pos 4, and O pos 2 -> pos 3 so, advance but choose the min of both values.
@@ -80,34 +77,37 @@ wordLetters.pos4.x = max (wordLetters.pos4.x - (wordLetters.abs.xtranslate*rando
   printLetter4()
 }
 
-//Example: movingY = 400 + 20 * sin(frameCount * 0.05);
+//Example reference for me 
+// movingY = 400 + 20(amplitude) * sin(frameCount * 0.05(speed));
 
 function printLetter1() { // starting position 1, "w"
   push();
-  textSize(12)
+  fill (myColors.text1.r, myColors.text1.g, myColors.text1.b)
+  textSize(wordLetters.abs.textsize + letterWiggle.s.amp * sin(frameCount * letterWiggle.s.speed)) // scale increases and decreases with sine wave
   text('w', wordLetters.pos1.x, wordLetters.pos1.y + letterWiggle.y.amp * sin(frameCount*letterWiggle.y.speed)) // the y coordinate moves up and down on the sine wave.
   pop();
 }
 
 function printLetter2() { // starting position 2, "o"
   push();
-  textSize(12)
+  fill (myColors.text2.r, myColors.text2.g, myColors.text2.b)
+  textSize(wordLetters.abs.textsize + letterWiggle.s.amp * cos(frameCount * letterWiggle.s.speed)) // variation with cosine
   text('o',wordLetters.pos2.x, wordLetters.pos2.y + letterWiggle.y.amp * cos(frameCount*letterWiggle.y.speed)*-1) // adding some variation by switching function
   pop();
 }
 
 function printLetter3() { // starting position 3, "n"
   push();
-  textSize(12)
+  fill (myColors.text2.r, myColors.text2.g, myColors.text2.b)
+  textSize(wordLetters.abs.textsize + letterWiggle.s.amp * sin(frameCount * letterWiggle.s.speed)*-1) // variation with negative
   text('n',wordLetters.pos3.x, wordLetters.pos3.y + letterWiggle.y.amp * sin(frameCount*letterWiggle.y.speed)*-1) // same thing
   pop();
 }
 
 function printLetter4() { // starting position 4, "k"
   push();
-  textSize(12)
-  text('k',wordLetters.pos4.x, wordLetters.pos4.y + letterWiggle.y.amp * cos(frameCount*letterWiggle.y.speed)) // yay
+  fill (myColors.text1.r, myColors.text1.g, myColors.text1.b)
+  textSize(wordLetters.abs.textsize + letterWiggle.s.amp * cos(frameCount * letterWiggle.s.speed)*-1) // variation cosine + negative
+  text('k',wordLetters.pos4.x, wordLetters.pos4.y + letterWiggle.y.amp * cos(frameCount*letterWiggle.y.speed)) // and son on
   pop();
 }
-
-
